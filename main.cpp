4d2dcab937e77ad460aa26875227eaef626e7814
vuxii <118520936+vuxii@users.noreply.github.com>
2023-10-05 20:04:55 +0300

    x3
    it works btw

---
+++ main.cpp
@@ -0,0 +1,16 @@
+# include "meow.hh"
+
+MIUMAU main uwaa meeoowh awuu tWt
+    MEEOOW meow XD
+    uwu meow XD
+    owo meow XD
+    nyan uwaa meow smawwer awuu tWt
+        miu uwaa meow wut awuu tWt
+            meow grrr XD
+        TwT mau tWt
+            meow GRRR XD
+            meow HURRRDURRR XD
+        TwT
+        owo " " AAH meow XD
+    TwT
+TwT
---
+++ meow.hh
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nyan while
+#define nya for
+#define miu if
+#define mau else
+typedef void meeoowh;
+typedef long long int MEEOOW;
+typedef int MIUMAU;
+#define grrr /=2
+#define GRRR *=3
+#define HURRRDURRR +=1
+#define wut %2==0
+#define smawwer >1
+#define uwu cin >>
+#define owo cout <<
+#define AAH <<
+#define XD ;
+#define PLZZ endl
+#define uwaa (
+#define awuu )
+#define tWt {
+#define TwT }
